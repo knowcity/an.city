7abedd274411c81136edd7408633e6e0880848be
Vincent at Neptune <vfengshen@gmail.com>
2017-12-09 15:09:15 +0800

    simplified vertical centering

--- ddh/app.js
+++ ddh/app.js
@@ -6,17 +6,9 @@
     	return void c.setAttribute("style", "height: 100vh; width: 100vw; background: #eee url(./assets/loading/desktop.png) no-repeat center 100px; background-size: 639px 396px;");
     }
 
+
 // for lock orientation
-	function moveLoader(n) { document.getElementById('intro-container').style.top=n.toString()+"px" }
-	function lockOrientation() {
-		if (window.innerWidth >= window.innerHeight) {
-			moveLoader(80);
-		} else {
-			moveLoader(488);
-		}
-	}
-	lockOrientation();
-	window.addEventListener("resize", lockOrientation);
+	// window.addEventListener("resize", lockOrientation);
 
 
 // renderer.backgroundColor = 0x3498db;
